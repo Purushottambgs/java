b876a73d724a8f3eb6e563c47a030441e7e63684
Purushottambgs <153331973+Purushottambgs@users.noreply.github.com>
2025-03-22 23:45:49 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("your name");
+       myname();
+       System.out.println("22");
+
+    }
+
+    static void myname(){
+        System.out.println("Purushottam Kumar");
+        System.out.println("my age is");
+    }
+}
---
+++ avg.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class avg {
+    public static void main(String[] agrs){
+     average(4,6);
+System.out.println("hello koun");
+average(8,9);
+    }
+
+    public static void average(int a, int b){
+        int avg=(a+b)/2;
+        System.out.println("average element is "+avg);
+    }
+}
